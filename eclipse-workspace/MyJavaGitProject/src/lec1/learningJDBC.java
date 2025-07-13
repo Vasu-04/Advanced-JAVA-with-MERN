cfd0e78475ea14e4f993a0a2cfceca52bfee0581
Himanshu Jindal <164390289+Vasu-04@users.noreply.github.com>
2025-07-13 14:24:17 +0530

    Commit Changes 1

--- eclipse-workspace/MyJavaGitProject/src/lec1/learningJDBC.java
+++ eclipse-workspace/MyJavaGitProject/src/lec1/learningJDBC.java
@@ -2,6 +2,7 @@ package lec1;
 
 public class learningJDBC {
 	public static void main(String[] args) {
-		System.out.println("Hello JDBC");
+		System.out.println("Hello JDBC.");
+		System.out.println("Good Afternoon");
 	}
 }
