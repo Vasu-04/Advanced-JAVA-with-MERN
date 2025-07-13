8d5335a367fdf283a7c1d835956398138101a804
Himanshu Jindal <164390289+Vasu-04@users.noreply.github.com>
2025-07-13 16:15:38 +0530

    Commit

--- eclipse-workspace/MyJavaGitProject/src/lec1/learningJDBC.java
+++ eclipse-workspace/MyJavaGitProject/src/lec1/learningJDBC.java
@@ -3,8 +3,8 @@ package lec1;
 public class learningJDBC {
 	public static void main(String[] args) {
 		System.out.println("Hello JDBC.");
-		System.out.println("Good Afternoon");
 		System.out.print(false);
 		System.out.print("bye");
+		
 	}
 }
